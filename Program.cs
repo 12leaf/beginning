38a1c2c985b903c1dc4108009d7488182f3d6ea6
Frank, Lea <lea.frank@tecalliance.net>
2023-09-13 15:14:02 +0200

    do while loop

--- Program.cs
+++ Program.cs
@@ -7,23 +7,23 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-          List<int> myNumberList = new List<int>();
-          Console.WriteLine("Wie viele Zahlen in die Liste hinzufügen");
-          int amountNumbers = Convert.ToInt32(Console.ReadLine());
-
-          while(myNumberList.Count < amountNumbers)
-          {
+        List<int> myNumberList = new List<int>();
+        int user_zahl=0;
+        do
+        {
+            //code block
             Console.WriteLine("Gebe mir eine Zahl");
-            myNumberList.Add(Convert.ToInt32(Console.ReadLine()));
-          }
-
-          //Summiere alle Zahlen auf
+            int user_zahl = Convert.ToInt32(Console.ReadLine());
+            myNumberList.Add(user_zahl);
+        }
+        while(user_zahl>0);
+                  //Summiere alle Zahlen auf
           int ergebnis = 0;
-          foreach(int zahl in myNumberList)
-          {
-            ergebnis+= zahl;
-          }
-            Console.WriteLine($"Das ergebnis ist {ergebnis}");
+        foreach(int zahl in myNumberList)
+         {
+         ergebnis+= zahl;
+             }
+        Console.WriteLine($"Das ergebnis ist {ergebnis}");
         }
     }
 }
