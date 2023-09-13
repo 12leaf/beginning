c69a85f8e26bddb66034ad057009ab97e791c4e6
Frank, Lea <lea.frank@tecalliance.net>
2023-09-13 15:52:42 +0200

    while2

--- Program.cs
+++ Program.cs
@@ -7,15 +7,20 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-        
+        List<int> myNumberList = new List<int>();
         Console.WriteLine("Bis zu welcher Zahl soll aufsummiert werden?");
         int untilNumber = Convert.ToInt32(Console.ReadLine());
-        
+
+        int i=0;
         int ergebnis= 0;
-        for(int i=0; i<=untilNumber; i++)
+        int zahl=0;
+
+        while(i<untilNumber)
         {
-            ergebnis+=i;
+            zahl = i+1;
+            ergebnis = ergebnis + zahl;
             Console.WriteLine($"Added numer{i}, current result {ergebnis}");
+            i++;
         }
        
 
