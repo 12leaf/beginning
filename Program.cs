752c8730610deb8301ae2f434261f54248cfd0ed
Frank, Lea <lea.frank@tecalliance.net>
2023-09-13 14:31:23 +0200

    if und else

--- Program.cs
+++ Program.cs
@@ -1,80 +1,28 @@
-﻿namespace HelloWorld
+﻿using System.IO.Pipes;
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            int zahl1 = 20;
-            int zahl2 = 30;
+           Console.WriteLine("Gebe mir deinen Namen");
+           string name = Console.ReadLine();
 
-            int ergebnis1 = zahl1 + zahl2;
-            Console.WriteLine(ergebnis1);
-
-            int ergebnis2 = zahl1 - zahl2;
-            Console.WriteLine(ergebnis2);
-
-            int ergebnis3 = zahl1 * zahl2;
-            Console.WriteLine(ergebnis3);
-
-            int ergebnis4 = zahl1 / zahl2;
-            Console.WriteLine(ergebnis4);
-
-            int ergebnis5 = zahl1 % zahl2;
-            Console.WriteLine(ergebnis5);
-
-            int ergebnis6 = ++ zahl1;
-            Console.WriteLine(ergebnis6);
-
-            int ergebnis7 = ++ zahl2;
-            Console.WriteLine(ergebnis7);
-
-            int ergebnis8 = -- zahl1 ;
-            Console.WriteLine(ergebnis8);
-
-            int ergebnis9 = -- zahl2;
-            Console.WriteLine(ergebnis9);
-
-            int ergebnis10 = zahl1 += 10;
-            Console.WriteLine(ergebnis10);
-
-            int ergebnis11 = zahl2 += 20;
-            Console.WriteLine(ergebnis11);
-
-            double zahl3 = 5.5;
-            double zahl4 = 1.2;
-
-            double ergebnis12 = zahl3 + zahl4;
-            Console.WriteLine(ergebnis12);
-
-            double ergebnis13 = zahl3 / zahl4;
-            Console.WriteLine(ergebnis13);
-
-            double ergebnis14 = ++ zahl3 ;
-            Console.WriteLine(ergebnis14);
-
-            float zahl5 = 79.46789536f;
-            float zahl6 = 63.892939f;
-
-            float ergebnis15 = zahl5 / zahl6;
-            Console.WriteLine(ergebnis15);
-
-            float ergebnis16 = ++ zahl5;
-            Console.WriteLine(ergebnis16);
-
-            float ergebnis17 = ++ zahl6;
-            Console.WriteLine(ergebnis17);
-
+           if(name.Length > 10)
+           {
+            Console.WriteLine("Oh der Name ist aber lang");
+           }
+            else if (name.Length > 5)
+            {
+                Console.WriteLine("der Name ist mittellang");
+            }
+            else
+            {
+                Console.WriteLine("Der ist ja kurz");
+            }
             
-
-
-
-           
-
-         
-
-            
-
 
         }
     }
