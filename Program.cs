21220266a18ed111b74462d0408cf4c1580b9879
Frank, Lea <lea.frank@tecalliance.net>
2023-09-13 13:10:37 +0200

    added exercise

--- Program.cs
+++ Program.cs
@@ -4,25 +4,71 @@
     {
         static void Main(string[] args)
         {
-            string text="Das ist ein test";
-            Console.WriteLine(text);
-            double kommazahl=2.1;
-            Console.WriteLine(kommazahl);
-            float flieskomma=26.3F;
-            Console.WriteLine(flieskomma);
-            decimal fliesskomma=26.5M;
-            Console.WriteLine(fliesskomma);
-            bool wert=true;
-            Console.WriteLine(wert);
-            bool wert2=false;
-            Console.WriteLine(wert2);
-            char buchstabe =(char)7;
-            Console.WriteLine(buchstabe);
-            long zahl=1729371829;
-            Console.WriteLine(zahl);
-            uint zahl2=5678;
-            Console.WriteLine(zahl2);
+
+            int zahl1 = 20;
+            int zahl2 = 30;
+
+            int ergebnis1 = zahl1 + zahl2;
+            Console.WriteLine(ergebnis1);
+
+            int ergebnis2 = zahl1 - zahl2;
+            Console.WriteLine(ergebnis2);
+
+            int ergebnis3 = zahl1 * zahl2;
+            Console.WriteLine(ergebnis3);
+
+            int ergebnis4 = zahl1 / zahl2;
+            Console.WriteLine(ergebnis4);
+
+            int ergebnis5 = zahl1 % zahl2;
+            Console.WriteLine(ergebnis5);
+
+            int ergebnis6 = ++ zahl1;
+            Console.WriteLine(ergebnis6);
+
+            int ergebnis7 = ++ zahl2;
+            Console.WriteLine(ergebnis7);
+
+            int ergebnis8 = -- zahl1 ;
+            Console.WriteLine(ergebnis8);
+
+            int ergebnis9 = -- zahl2;
+            Console.WriteLine(ergebnis9);
+
+            int ergebnis10 = zahl1 += 10;
+            Console.WriteLine(ergebnis10);
+
+            int ergebnis11 = zahl2 += 20;
+            Console.WriteLine(ergebnis11);
+
+            double zahl3 = 5.5;
+            double zahl4 = 1.2;
+
+            double ergebnis12 = zahl3 + zahl4;
+            Console.WriteLine(ergebnis12);
+
+            double ergebnis13 = zahl3 / zahl4;
+            Console.WriteLine(ergebnis13);
+
+            double ergebnis14 = ++ zahl3 ;
+            Console.WriteLine(ergebnis14);
+
+            float zahl5 = 79.46789536f;
+            float zahl6 = 63.892939f;
+
+            float ergebnis15 = zahl5 / zahl6;
+            Console.WriteLine(ergebnis15);
+
             
+
+
+
+           
+
+         
+
+            
+
 
         }
     }
