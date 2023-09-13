0abfef0c258b5debc2317d2b7d72096da4414f3d
Frank, Lea <lea.frank@tecalliance.net>
2023-09-13 11:12:21 +0200

    added substraction

--- Program.cs
+++ Program.cs
@@ -4,16 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gebe mir eine erste Zahl!");
+            Console.WriteLine("Gib eine erste Zahl an!");
             int zahl1 = Convert.ToInt16(Console.ReadLine());
 
-            Console.WriteLine("Gebe mir eine zweite Zahl!");
+            Console.WriteLine("Gib eine zweite Zahl an!");
             int zahl2 = Convert.ToInt16(Console.ReadLine());
 
-            int ergebnis = zahl1 + zahl2;
+            int ergebnis = zahl1 - zahl2;
 
-            
-            Console.WriteLine($"Das Ergebnis aus {zahl1} und {zahl2} ist {ergebnis}" );
+            Console.WriteLine($"Das Ergebnis aus {zahl1} und {zahl2} ist {ergebnis}");
+
         }
     }
 }
