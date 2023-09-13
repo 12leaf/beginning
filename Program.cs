7ca13b2309829089688543ec477f920069630035
Frank, Lea <lea.frank@tecalliance.net>
2023-09-13 09:52:08 +0200

    added age question to programm

--- Program.cs
+++ Program.cs
@@ -6,8 +6,15 @@
         {
             Console.WriteLine("Gebe mir deinen Namen!");
             string name = Console.ReadLine();
-            
-            Console.WriteLine("Hello," + name);
+
+            Console.WriteLine("Sage mir wie alt du bist");
+            string age = Console.ReadLine();
+
+            int alter = Convert.ToInt16(age);
+
+            int alterInZehn = alter +10;
+
+            Console.WriteLine("Hello, " + name + " Dein Alter ist " + age + " in 10 Jahren bist du " + alterInZehn );
         }
     }
 }
