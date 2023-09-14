d45fce8f752b932d0008fb176bbe448748de90e5
Frank, Lea <lea.frank@tecalliance.net>
2023-09-14 10:55:58 +0200

    Aufgabe 9

--- Program.cs
+++ Program.cs
@@ -7,10 +7,17 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-        Console.WriteLine("Gib einen Text ein!");
+        Console.WriteLine("Gib einen Text mit Semikolon ein!");
         string text = Console.ReadLine();
         
-        Console.WriteLine(text.Contains("abc"));
+        string[] text2 = text.Split(";");
+
+        foreach(string abc in text2)
+        {
+            Console.WriteLine(abc);
+        }
+
+        Console.WriteLine(text2);
         }
     }
 }
