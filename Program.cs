ff1892ed9ea580999521434ea1183fb9ee4d1239
Frank, Lea <lea.frank@tecalliance.net>
2023-09-14 08:05:41 +0200

    Aufgabe 2

--- Program.cs
+++ Program.cs
@@ -7,11 +7,8 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-       Console.WriteLine("Nenne eine Zahl!");
-       int zahl1 = Convert.ToInt32(Console.ReadLine());
-
-       Console.WriteLine("Nenne eine zweite Zahl!");
-       int zahl2 = Convert.ToInt32(Console.ReadLine());
+       int zahl1 = 5;
+       int zahl2 = 6;
 
        int ergebnis = zahl1 + zahl2;
        Console.WriteLine($"Das ergebnis aus {zahl1} und {zahl2} ist {ergebnis}");
